da2a764b0156a593d7695c58853d8fe3af1920f9
Gregory Soares <gregory16704@gmail.com>
2025-05-10 15:11:29 -0300

    feat(rotas): adicionando esqueleto bruto das telas de rota sem js ou style

--- src/pages/landingPage/ServicedRoutes.jsx
+++ src/pages/landingPage/ServicedRoutes.jsx
@@ -1,4 +1,5 @@
-import { Button, ButtonText, Image, InputRoot, InputField, InputIcon, InputLabel, Section, Shape } from "@/components";
+import { Button, ButtonText, Image, InputRoot, InputField, InputIcon, InputLabel, InputMessage, Section, Shape } from "@/components";
+import { CaretDown, MapPin, Package } from "phosphor-react";
 
 export function ServicedRoutes() {
   return (
@@ -6,6 +7,59 @@ export function ServicedRoutes() {
       <div className="bg-white">
         <Section>
           <h2>Rotas atendidas</h2>
+          <p>Aguardando texto produzido por PO, até lá, estará vazio, assim como a contribuição do PO</p>
+          <div className="flex justify-evenly font-bold">
+            <p className="border-b-3 border-red-tx">Mapa</p>
+            <p>Ver lista</p>
+          </div>
+          <div>
+            <InputLabel>Cidade de origem</InputLabel>
+            <InputRoot>
+              <InputIcon>
+                <MapPin className="icon"/>
+              </InputIcon>
+              <InputField placeholder="Ex: Paranavaí"/>
+            </InputRoot>
+            <InputMessage>Atendemos essa cidade!</InputMessage>
+            <InputLabel>Cidade de destino</InputLabel>
+            <InputRoot>
+              <InputIcon>
+                <MapPin className="icon"/>
+              </InputIcon>
+              <InputField placeholder="Ex: Maringá"/>
+            </InputRoot>
+            <InputMessage>Cidade fora da rota!</InputMessage>
+          </div>
+          <Button className="bg-red-tx">
+            <Package className="icon text-white"/>
+            <ButtonText className="text-white">
+              Solicitar orçamento
+            </ButtonText>
+          </Button>
+          <div className="flex justify-evenly font-bold">
+            <p>Mapa</p>
+            <p className="border-b-3 border-red-tx">Ver lista</p>
+          </div>
+          <div className="bg-gray-50 rounded-2xl p-6 font-bold flex flex-col gap-y-4">
+            <p className="flex">
+              <MapPin className="icon mr-4"/> Cidade 1
+            </p>
+            <p className="flex">
+              <MapPin className="icon mr-4"/> Cidade 2
+            </p>
+            <p className="flex">
+              <MapPin className="icon mr-4"/> Cidade 3
+            </p>
+            <p className="flex">
+              <MapPin className="icon mr-4"/> Cidade 4
+            </p>
+          </div>
+          <Button className="bg-red-tx">
+            <Package className="icon text-white"/>
+            <ButtonText className="text-white">
+              Solicitar orçamento
+            </ButtonText>
+          </Button>
         </Section>
       </div>
     </>
