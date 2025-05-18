31f8667dc82ae977cdb24eb4ae7a6be902ee0c17
PedrooLino <ph583152@gmail.com>
2025-05-18 20:30:09 -0300

    refactor(sobre):corrige layout do carrossel e ajusta imagens

--- src/pages/landingPage/Carousel.jsx
+++ src/pages/landingPage/Carousel.jsx
@@ -16,25 +16,36 @@ export function CarouselPlugin() {
     Autoplay({ delay: 2000, stopOnInteraction: false })
   )
 
+  const carImg = [
+    "./src/assets/car01.webp",
+    "./src/assets/car02.webp",
+    "./src/assets/car03.jpg",
+    "./src/assets/car04.jpg",
+    "./src/assets/car05.jpg",
+  ];
+
   return (
     <Carousel
       plugins={[plugin.current]}
-      className="w-full h-20"
+      className=" w-full"
       onMouseEnter={plugin.current.stop}
       onMouseLeave={plugin.current.reset}
     >
+      
+      
       <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
+        {carImg.map((src, index) => (
           <CarouselItem key={index}>
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex items-center justify-center px-0" >
-                  <Image src="./src/assets/car04.jpg"/>
+            <div className="p-1 ">
+              <Card className="px-0 py-0 gap-6 ">
+                <CardContent className="flex items-center justify-center py-0 px-0  " >
+                  <Image src={src} className=" aspect-[16/9] object-cover w-full h-full "/>
                   
                 </CardContent>
               </Card>
             </div>
           </CarouselItem>
+          
         ))}
       </CarouselContent>
       <CarouselPrevious />
