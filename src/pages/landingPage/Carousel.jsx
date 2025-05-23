9029ef1b60cdbfd3131209beb8ac4e992b3c955d
PedrooLino <ph583152@gmail.com>
2025-05-23 10:26:08 -0300

    refactor(sobre): ajustem no carousel

--- src/pages/landingPage/Carousel.jsx
+++ src/pages/landingPage/Carousel.jsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import Autoplay from "embla-carousel-autoplay"
+import { Button, ButtonText, Image, InputRoot, InputField, InputIcon, InputLabel, Section, Shape } from "@/components";
 
 import { Card, CardContent } from "@/components/ui/card"
 import {
@@ -15,28 +16,41 @@ export function CarouselPlugin() {
     Autoplay({ delay: 2000, stopOnInteraction: false })
   )
 
+  const carImg = [
+    "./src/assets/car01.webp",
+    "./src/assets/car02.webp",
+    "./src/assets/car03.jpg",
+    "./src/assets/car04.jpg",
+    "./src/assets/car05.jpg",
+  ];
+
   return (
     <Carousel
       plugins={[plugin.current]}
-      className="w-full h-20"
+      className=" w-full"
       onMouseEnter={plugin.current.stop}
       onMouseLeave={plugin.current.reset}
     >
+      
+      
       <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
+        {carImg.map((src, index) => (
           <CarouselItem key={index}>
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
+            <div className="p-1 ">
+              <Card className="border-0 px-0 py-0 gap-6 ">
+                <CardContent className="flex items-center justify-center py-0 px-0  " >
+                  <Image src={src} className=" aspect-[16/7] object-cover w-full h-full "/>
+                  
                 </CardContent>
               </Card>
             </div>
           </CarouselItem>
+          
         ))}
       </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
+      
+      <CarouselPrevious className="cursor-pointer  bg-white hidden md:block" />
+      <CarouselNext className="cursor-pointer  bg-white  hidden md:block "/>
     </Carousel>
   )
 }
