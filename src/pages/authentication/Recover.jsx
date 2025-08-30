2960da5a4f7876fcbaa1c62efdfe0ef0b0bf3570
PedroLino <ph583152@gmail.com>
2025-08-30 19:45:58 -0300

    feat(recuperar):cria as telas para a recuperaçao de senha

--- src/pages/authentication/Recover.jsx
+++ src/pages/authentication/Recover.jsx
@@ -1,7 +1,7 @@
 import { Button, ButtonText, Image, InputRoot, InputField, InputIcon, InputLabel, InputMessage, Section, Shape } from "@/components";
 import { Eye, EyeSlash, UserList, LockSimpleOpen, CheckCircle } from "phosphor-react";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useState, useEffect } from "react";
+import { useForm, Controller } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -16,32 +16,209 @@ import {
 } from "@/components/ui/input-otp"
 
 export function Recover() {
+  
+  const [showCodeScreen, setShowCodeScreen] = useState(false);
+
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: zodResolver(recoverSchema),
+    mode: "onBlur"
+  });
+
+  const onSubmit = (data) => {
+    console.log("enviado...");
+    setShowCodeScreen(true);
+  };
+
 
   return (
     <>
+
+      {showCodeScreen ? (
+        <Code />
+      ) : (
       <SectionBox className="pt-0">
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <CloudinaryImage publicId="vfq6dw8u2de9vcybxvka" className="w-64 justify-self-center" />
         <h4 className="font-bold text-center py-7 cursor-default">Recupere sua conta</h4>
-        <div className="flex flex-col items-center">
-          <InputOTP maxLength={6}>
-          <InputOTPGroup>
-            <InputOTPSlot index={0} />
-            <InputOTPSlot index={1} />
-            <InputOTPSlot index={2} />
-          </InputOTPGroup>
-          <InputOTPSeparator />
-          <InputOTPGroup>
-            <InputOTPSlot index={3} />
-            <InputOTPSlot index={4} />
-            <InputOTPSlot index={5} />
-          </InputOTPGroup>
-          </InputOTP>
+        <div className="flex flex-col mt-6">
+          <InputLabel>Email</InputLabel>
+          <InputRoot>
+            <InputField 
+              placeholder="Digite seu email" {...register("email")}/>
+          </InputRoot>
+          <InputMessage className="text-danger-base">{errors.email?.message}</InputMessage>
+          <Button type="submit" className="bg-red-tx text-center mt-4">
+            <ButtonText className="text-white text-center"> 
+              Enviar código
+            </ButtonText>
+          </Button>
         </div>
-      </SectionBox>
+      </form>
+    </SectionBox>
+      )}
     </>
   );
 
 }
+
+function Code(){
+
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    resolver: zodResolver(codeSchema),
+    mode: "onBlur"
+  });
+
+  const [showPasswordScreen, setShowPasswordScreen] = useState(false);
+  
+  const onSubmitCode = (data) => {
+    console.log("Código válido:");
+    setShowPasswordScreen(true);
+  };
+  return (
+    <>
+
+    {setShowPasswordScreen ? (
+        <PasswordScreen />
+      ) : (
+      <SectionBox className="pt-0">
+        <form onSubmit={handleSubmit(onSubmitCode)} noValidate>
+        <CloudinaryImage publicId="vfq6dw8u2de9vcybxvka" className="w-64 justify-self-center" />
+        <h4 className="font-bold text-center py-7 cursor-default">Recupere sua conta</h4>
+        <div className="flex flex-col items-center">
+        <div className="flex flex-col ">
+          <InputLabel className=" text-left">Código</InputLabel>
+          <Controller
+              name="code"
+              control={control}
+              render={({ field }) => (
+          <InputOTP maxLength={6} {...field}>
+          <InputOTPGroup>
+            <InputOTPSlot index={0} />
+          </InputOTPGroup>
+          <InputOTPGroup>
+            <InputOTPSlot index={1} />
+          </InputOTPGroup>
+          <InputOTPGroup>
+            <InputOTPSlot index={2} />
+          </InputOTPGroup>
+          <InputOTPSeparator className="invisible"/>
+          <InputOTPGroup>
+            <InputOTPSlot index={3} />
+          </InputOTPGroup>
+          <InputOTPGroup>
+            <InputOTPSlot index={4} />
+          </InputOTPGroup>
+          <InputOTPGroup>
+            <InputOTPSlot index={5} />
+          </InputOTPGroup>
+          </InputOTP>
+              )} />
+          <InputMessage className="text-danger-base">{errors.code?.message}</InputMessage>
+          <Countdown />
+          <Button className="bg-red-tx text-center">
+            <ButtonText className="text-white text-center"> 
+              Confirmar
+            </ButtonText>
+          </Button>
+          <p className="text-center leading-none"><span className="text-sm">Código enviado para</span><br/>
+          <span className="text-xs ">cliente@gmail.com</span></p>
+          <p className="text-center mt-1"><span className="text-base font-bold">Mudar email</span></p>
+        </div>
+        </div>
+        </form>
+        
+      </SectionBox>
+      )}
+    </>
+  );
+}
+
+function PasswordScreen(){
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    resolver: zodResolver(passwordSchema),
+    mode: "onBlur"
+  });
+
+ 
+  
+  const onSubmitPassword = (data) => {
+    console.log("Código válido:");
+    
+  };
+  return (
+    <>
+      <SectionBox className="pt-0">
+          <form onSubmit={handleSubmit(onSubmitPassword)} noValidate>
+            <CloudinaryImage publicId="vfq6dw8u2de9vcybxvka" className="w-64 justify-self-center" />
+            <h4 className="font-bold text-center py-7 cursor-default">Recupere sua conta</h4>
+            <FormField
+              register={register}
+              name="password"
+              title="Nova senha"
+              placeholder="Digite a senha nova"
+              dirty={touchedFields.password}
+              error={errors.password}
+              onChangeMask={(v) => maskInput(v, "password")}
+              icon={UserList}
+            />
+            <FormField
+              register={register}
+              name="newPassword"
+              title="Confirmar nova senha"
+              placeholder="Confirme senha"
+              dirty={touchedFields.password}
+              error={errors.newPassword}
+              onChangeMask={(v) => maskInput(v, "password")}
+              icon={UserList}
+            />
+
+            <Button className="bg-red-tx text-center">
+            <ButtonText className="text-white text-center"> 
+              salvar
+            </ButtonText>
+          </Button>
+          </form>
+      </SectionBox>
+    </>
+  )
+}
+
+const Countdown = () => {
+  const [seconds, setSeconds] = useState(60); 
+  const [isActive, setIsActive] = useState(true);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive && seconds > 0) {
+      interval = setInterval(() => {
+        setSeconds(prevSeconds => prevSeconds - 1);
+      }, 1000);
+    } else if (seconds === 0) {
+      setIsActive(false);
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, seconds, isActive]);
+
+  
+  const handleResend = () => {
+    setSeconds(60);
+    setIsActive(true);
+  };
+
+  return (
+    <p
+      className={`text-center font-bold my-2  ${seconds === 0 ? "cursor-pointer" : ""}`}
+      onClick={seconds === 0 ? handleResend : null}
+    ><span className="text-base">
+      {seconds === 0 ? "Reenviar código" : `Enviar código novamente (${seconds}s)`}
+      </span>
+    </p>
+  );
+};
+
 
 function FormField({ title, placeholder, register, name, error, dirty, type = "text", icon: Icon, onChangeMask, autoComplete = "off" }) {
   let status;
@@ -99,19 +276,29 @@ function maskInput(value) {
   }
 }
 
-const loginSchema = z.object({
-  cpf_cnpj: z
+const recoverSchema = z.object({
+  email: z
     .string()
-    .nonempty("Campo obrigatório")
-    .refine((val) => {
-      const digits = val.replace(/\D/g, "");
-      if (digits.length === 11) return cpf.isValid(digits);
-      if (digits.length === 14) return cnpj.isValid(digits);
-      return false;
-    }, { message: "CPF ou CNPJ inválido" }),
+    .nonempty("O campo de email não pode ser vazio.")
+    .email("Por favor, digite um email válido."),
+});
 
+const codeSchema = z.object({
+  code: z
+    .string()
+    .nonempty("O campo não pode ser vazio.")
+    .length(6, "Por favor, preencha todos os 6 dígitos."),
+});
+
+const passwordSchema = z.object({
   password: z
     .string()
     .nonempty("Campo obrigatório")
-    .min(8, "Mínimo de 8 caracteres")
+    .min(8, "Mínimo de 8 caracteres"),
+  newPassword: z
+    .string()
+    .nonempty("Confirme sua senha"),
+}).refine(data => data.password === data.newPassword, {
+  message: "As senhas não coincidem.",
+  path: ["newPassword"],
 });
