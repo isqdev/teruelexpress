d1465e0b46ce745d962fa0e0bef6b62bb8621418
Isaque Dias <isaque.brain@gmail.com>
2025-08-30 19:31:05 -0300

    refactor(configuracoes da conta): implementa formulários

--- src/pages/app/screens/Settings.jsx
+++ src/pages/app/screens/Settings.jsx
@@ -1,218 +1,166 @@
-import { Button, ButtonText, InputRoot, InputField, InputIcon, InputLabel, InputMessage, AppHeader, 
-  SectionApp, Shape, ModalConfirm, ModalSm } from "@/components";
-import { Eye, EyeSlash, UserList, LockSimpleOpen, CheckCircle, Star, Package, HouseLine, StarHalf, 
-  StarFour, FolderSimpleStar, Pencil, FloppyDiskBack, LockKeyOpen } from "phosphor-react";
-import { useState, useEffect } from "react";
-import { MyReviews } from "./MyReviews";
+import { Button, ButtonText, InputRoot, InputField, InputIcon, InputLabel, InputMessage, AppHeader,SectionApp } from "@/components";
+import { CheckCircle, Pencil, FloppyDiskBack, LockKeyOpen } from "phosphor-react";
+import { useState } from "react";
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm, Controller } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 
 export function Settings() {
 
   const {
-    register,
-    handleSubmit,
-    control,
-    formState: { errors: errorsEdit },
-  } = useForm({
-    resolver: zodResolver(schemaEdit),
-  });
+          register,
+          handleSubmit,
+          formState: { errors, touchedFields }
+      } = useForm({
+          resolver: zodResolver(schemaEditData),
+          mode: "onBlur",
+          defaultValues: {
+            name: "João Silva", 
+            email: "joao@email.com", 
+            phone: "(11) 9 8765-4321" 
+          }
+      });
 
   const {
-    register: registerPassword,
-    handleSubmit: handleSubmitPassword,
-    formState: { errors: errorsPassword },
-  } = useForm({
-    resolver: zodResolver(schemaPassword),
-  });
+          register: registerPassword,
+          handleSubmit: handleSubmitPassword,
+          reset: resetPassword,
+          formState: { errors: errorsPassword, touchedFields: touchedFieldsPassword }
+      } = useForm({
+          resolver: zodResolver(schemaPassword),
+          mode: "onBlur"
+      });
 
-
-  const [isEditing, setIsEditing]= useState(false);
-  const [isPassword, setIsPassword]= useState(false);
-  const [items, setItems] = useState({});
-
-  
-
-  useEffect(() => {
-    loadReviewsLocalStorage();
-  }, []);
-
-  const loadReviewsLocalStorage = () => {
-      try {
-        const storedReviews = localStorage.getItem('JSON enviado');
-        if (storedReviews) {
-          const reviews = JSON.parse(storedReviews);
-
-         
-          const itemsUser = {
-            
-            nomeCliente: reviews.namePerson || reviews.nameBusiness, 
-            email: reviews.email,
-            telefone: reviews.phone,
-            senha: reviews.password, 
-            documento:reviews.cpf || reviews.cnpj
-          };
-          setItems(itemsUser);
-        } 
-        
-      } catch (error) {
-        console.error("Erro ao carregar  'jsonReview' do localStorage:", error);
-        setItems();
-      }
-  }
-  
-  const saveToLocalStorage = (data) => {
-    console.log("Salvando dados de edição...");
-    try {
-      const storedReviews = localStorage.getItem('JSON enviado');
-      const reviews = storedReviews ? JSON.parse(storedReviews) : {};
-      reviews.namePerson = data.nome;
-      reviews.email = data.email;
-      reviews.phone = data.phone;
-      localStorage.setItem('JSON enviado2', JSON.stringify(reviews));
-      // setIsEditing(!isEditing);
-      loadReviewsLocalStorage();
-    } catch (error) {
-      console.error("Erro ao salvar dados no localStorage:", error);
-    }
-  };
-
-  const saveToLocalStoragePassword = (values) => {
-    console.log("Salvando nova senha...");
-    try {
-      const storedReviews = localStorage.getItem('JSON enviado');
-      const reviews = storedReviews ? JSON.parse(storedReviews) : {};
-      reviews.password = values.password;
-      localStorage.setItem('JSON enviado3', JSON.stringify(reviews));
-      setIsPassword(!isPassword);
-      loadReviewsLocalStorage();
-    } catch (error) {
-      console.error("Erro ao salvar dados no localStorage:", error);
-    }
-  };
+  const [isEditingData, setisEditingData]= useState(false);
+  const [isEditingPassword, setIsEditingPassword] = useState(false);
 
   const onSubmitEdit = (data) => {
-    saveToLocalStorage(data);
-  }
-
-  const onSubmitPassword = (values) => {
-    saveToLocalStoragePassword(values);
-  }
-
-
-  const toggleEdit = () => {
- 
-    setIsEditing(!isEditing);
-    
+    localStorage.setItem("user-data", JSON.stringify(data))
+    setisEditingData(false)
+    toast.success("Cadastro atualizado!")
   };
 
-
-  const togglePassword = () => {
-    setIsPassword(!isPassword);
-  }
-
-
-
+  const onSubmitPassword = (data) => {
+    localStorage.setItem("pass", JSON.stringify(data))
+    setIsEditingPassword(false);
+    resetPassword(); 
+    toast.success("Senha atualizada!")
+  };
 
   return (
     <>
       <SectionApp>
             <AppHeader screenTitle="Configurações"/>
-            <div className="sm:shadow-2xl sm:mx-auto sm:rounded-xl sm:p-6 sm:w-full 
-              sm:max-w-3xl sm:flex sm:justify-center sm:items-center">
+            <div className=" lg:max-w-3xl mx-auto mt-4">
             <div>
-            <div className="flex ">
+            <div className="flex items-center">
               <div className=" w-18 h-18 rounded-full  bg-gray-50 flex items-center justify-center text-center mr-3">
-                <Pencil className="icon " />
+                <Pencil size={42} />
               </div>
-              <div className="gap-0">
-                <p className="font-bold m-0 gap-0">Nome do cliente</p>
-                <span className="font-bold text-xs m-0 gap-0">pessoa fisica</span>
-                <p className="m-0 gap-0">000.000.000-00</p>
+              
+              <div className="flex flex-col">
+                <span className="font-bold text-base md:text-xl">Nome do cliente</span>
+                <span className="font-bold -mt-1 text-xs sm:text-sm">Pessoa física</span>
+                <span className="text-base -mt-1 md:text-xl">000.000.000-00</span>
               </div>
             </div>
-            <div className="sm:flex sm:gap-8">
-            <div className="border border-2 rounded-2xl border-gray-600 mt-4">
+            <div className="grid sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-4">
+            <div className="border rounded-2xl border-gray-600 mt-4 p-3 w-full">
               <form onSubmit={handleSubmit(onSubmitEdit)} className="m-2 ">
-                <p className="font-bold mb-5">Dados da conta</p>
-                <InputLabel>Nome</InputLabel>
-                <InputRoot className={!isEditing ? "bg-gray-50" : ""} >
-                  <InputField placeholder={items.nomeCliente} {...register("nome")}   
-                  disabled={!isEditing} defaultValue={items.nomeCliente} />
-                </InputRoot>
-                <InputLabel>Email</InputLabel>
-                <InputRoot className={!isEditing ? "bg-gray-50" : ""} >
-                  <InputField placeholder={items.email} defaultValue={items.email} 
-                  disabled={!isEditing} {...register("email")}/>
-                </InputRoot>
-                <InputLabel>Telefone</InputLabel>
-                <InputRoot className={!isEditing ? "bg-gray-50" : ""} >
-                  <Controller
-                    name="phone"
-                    control={control}
-                    render={({ field }) => (
-                      <InputField
-                        {...register("phone")}
-
-                        {...field}
-                        onChange={(e) => {
-                          field.onChange(maskInput(e.target.value));
-                        }}
-                        disabled={!isEditing}
-                      />
-                      )}
+                <h5 className="font-bold text-2xl">Dados da conta</h5>
+                  <FormField
+                    readOnly={!isEditingData}
+                    register={register}
+                    name={"name"}
+                    title="Nome"
+                    placeholder="Digite seu nome"
+                    error={errors.name}
+                    dirty={touchedFields.cep}
+                    type="text"
                   />
-                
-                </InputRoot>
-            <Button type={isEditing ? "submit" : "button"} onClick={!isEditing ? toggleEdit : null} className="ml-auto bg-blue-tx w-auto  mt-3">
-                  <ButtonText className="text-center text-white flex">
-                    {isEditing ? (
-                      <>
-                    <FloppyDiskBack className="icon mr-2" /> Salvar
-                      </> 
-                    ) : (
-                      <>
-                      <Pencil className="icon mr-2" /> Editar 
-                      </>
-                    )}
-                  </ButtonText>
-                </Button>
+                  <FormField
+                    readOnly={!isEditingData}
+                    register={register}
+                    name={"email"}
+                    title="Email"
+                    placeholder="Digite seu email"
+                    error={errors.email}
+                    dirty={touchedFields.cep}
+                    type="email"
+                  />
+                  <FormField
+                    readOnly={!isEditingData}
+                    register={register}
+                    name={"phone"}
+                    title="Telefone"
+                    placeholder="Digite seu número"
+                    error={errors.phone}
+                    dirty={touchedFields.cep}
+                    type="text"
+                    onChangeMask={(v) => maskInputPhone(v)}
+                  />
+                    <Button type="submit" variant="complementary" className={`ml-auto w-auto mt-3 ${!isEditingData && "hidden"}`}>
+                      <FloppyDiskBack className="icon " />
+                      <ButtonText >
+                          Salvar
+                      </ButtonText>
+                    </Button>       
+                    <Button type="button" variant="complementary" className={`ml-auto w-auto mt-3 ${isEditingData && "hidden"}`} onClick={() => setisEditingData(true)}>
+                      <Pencil className="icon " /> 
+                      <ButtonText >
+                          Editar
+                      </ButtonText>
+                    </Button>
               </form>
             </div>
-            <div className="border border-2 rounded-2xl border-gray-600 mt-4">
+            <div className="border rounded-2xl border-gray-600 mt-4 w-full p-3">
               <form onSubmit={handleSubmitPassword(onSubmitPassword)} className="m-2 ">
-                <p className="font-bold mb-5">Senha</p>
-                <InputLabel>Senha atual</InputLabel>
-                <InputRoot className={!isEditing ? "bg-gray-100" : ""} >
-                  <InputField  disabled={!isPassword}/>
-                </InputRoot >
-                <InputLabel>Nova senha</InputLabel>
-                <InputRoot className={!isEditing ? "bg-gray-100" : ""}>
-                  <InputField disabled={!isPassword} {...registerPassword("password")} type="password" />
-                </InputRoot>
-                <InputLabel>Confirmar senha</InputLabel>
-                <InputRoot className={!isEditing ? "bg-gray-100" : ""}>
-                  <InputField disabled={!isPassword} {...registerPassword("confirmPassword")} type="password" />
-                </InputRoot>
-               
-
-                <Button type={isPassword ? "submit" : "button"} onClick={!isPassword ? togglePassword : null}  
-                  className="ml-auto bg-blue-tx w-auto  mt-3">
-                  <ButtonText className="text-center text-white flex">
-                    {isPassword ? (
-                      <>
-                    <FloppyDiskBack className="icon mr-2" /> Salvar
-                      </>
-                    ) : (
-                      <>
-                      <LockKeyOpen className="icon mr-2" /> Alterar senha
-                      </>
-                    )}
-                  </ButtonText>
-                </Button>
+                <h5 className="font-bold text-2xl">Senha</h5>
+                  <FormField
+                    readOnly={!isEditingPassword}
+                    register={registerPassword}
+                    name={"currentPassword"}
+                    title="Senha atual"
+                    placeholder="Digite sua senha atual"
+                    error={errorsPassword.currentPassword}
+                    dirty={touchedFieldsPassword.currentPassword}
+                    type="password"
+                  />
+                  <FormField
+                    readOnly={!isEditingPassword}
+                    register={registerPassword}
+                    name={"password"}
+                    title="Nova senha"
+                    placeholder="Digite sua nova senha"
+                    error={errorsPassword.password}
+                    dirty={touchedFieldsPassword.password}
+                    type="password"
+                  />
+                  <FormField
+                    readOnly={!isEditingPassword}
+                    register={registerPassword}
+                    name={"confirmPassword"}
+                    title="Confirmar nova senha"
+                    placeholder="Confirme sua nova senha"
+                    error={errorsPassword.confirmPassword}
+                    dirty={touchedFieldsPassword.confirmPassword}
+                    type="password"
+                  />
+                    <Button type="submit" variant="complementary" className={`ml-auto w-auto mt-3 ${!isEditingPassword && "hidden"}`}>
+                      <FloppyDiskBack className="icon " />
+                      <ButtonText >
+                          Salvar
+                      </ButtonText>
+                    </Button>       
+                    <Button type="button" variant="complementary" className={`ml-auto w-auto mt-3 ${isEditingPassword && "hidden"}`} onClick={() => setIsEditingPassword(true)}>
+                      <LockKeyOpen className="icon " /> 
+                      <ButtonText >
+                          Alterar senha
+                      </ButtonText>
+                    </Button>
               </form>
-              </div>
+            </div>
             </div> 
             </div>
             </div>
@@ -222,8 +170,8 @@ export function Settings() {
   );
 }
 
-const schemaEdit = z.object({
-  nome: z.string().nonempty("O nome é obrigatório"),
+const schemaEditData = z.object({
+  name: z.string().nonempty("O nome é obrigatório"),
   email: z
     .string()
     .email("Email inválido"),
@@ -233,15 +181,15 @@ const schemaEdit = z.object({
     .refine((val) => val.length === 10 || val.length === 11, { message: "Telefone inválido" }),
 });
 
-
 const schemaPassword = z.object({
+  currentPassword: z.string().nonempty("Campo obrigatório"),
   password: z
     .string()
-    .nonempty("Campo obrigatório")
+    .min(1, "Campo obrigatório")
     .min(8, "Mínimo de 8 caracteres")
-    .refine((val) => /[A-Z]/.test(val), { message: "Deve conter ao menos 1 letra maiúscula" })
-    .refine((val) => /[0-9]/.test(val), { message: "Deve conter ao menos 1 número" })
-    .refine((val) => /[@#$?]/.test(val), { message: "Deve conter ao menos 1 caractere especial (@, #, $, ?)" }),
+    .refine((val) => /[A-Z]/.test(val), { message: "Deve conter 1 letra maiúscula" })
+    .refine((val) => /[0-9]/.test(val), { message: "Deve conter 1 número" })
+    .refine((val) => /[@#$?]/.test(val), { message: "Deve conter 1 caractere especial" }),
   confirmPassword: z.string().nonempty("Campo obrigatório"),
 }).refine((data) => data.password === data.confirmPassword, {
   message: "As senhas não coincidem",
@@ -249,10 +197,9 @@ const schemaPassword = z.object({
 });
 
 
-function maskInput(value) {
+function maskInputPhone(value) {
   const onlyDigits = value.replace(/\D/g, '');
 
- 
     if (onlyDigits.length > 10) {
       return onlyDigits
         .replace(/^(\d{2})(\d{1})(\d{4})(\d{4}).*/, '($1) $2 $3-$4')
@@ -266,6 +213,42 @@ function maskInput(value) {
         if (!last) return `(${ddd}) ${first}`;
         return `(${ddd}) ${first}-${last}`;
       });
-  
+}
 
+function FormField({ title, placeholder="", register, name, error, dirty, type = "text", onChangeMask, readOnly=false }) {
+    console.log(`FormField ${name} - readOnly:`, readOnly); // Debug
+    
+    let status;
+    if (dirty) {
+        status = error ? "error" : "validated"
+    }
+
+    return (
+        <>
+            <InputLabel className="pt-4">{title}</InputLabel>
+            <InputRoot
+              status={status}
+              className={readOnly ? "bg-gray-50 pointer-events-none" : ""}
+            >
+              <InputField
+                readOnly={readOnly}
+                disabled={readOnly}
+                placeholder={placeholder}
+                type={type}
+                {...register(name, onChangeMask ? {
+                  onChange: (e) => {
+                    e.target.value = onChangeMask(e.target.value);
+                  }
+                } : {})}
+              />
+              {status === "validated" && (
+                <InputIcon>
+                  <CheckCircle size={32} className="text-success-base" />
+                </InputIcon>
+              )}
+            </InputRoot>
+
+            <InputMessage className="text-danger-base">{error?.message}</InputMessage>
+        </>
+    );
 }
