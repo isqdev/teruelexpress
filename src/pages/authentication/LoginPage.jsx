1636d430ae9888c4add4cfea0626600420fb34bf
MenoniJesus <menonifpr@gmail.com>
2025-10-11 17:29:48 -0300

    quickfix

--- src/pages/authentication/LoginPage.jsx
+++ src/pages/authentication/LoginPage.jsx
@@ -18,7 +18,7 @@ export function LoginPage() {
   const navigate = useNavigate();
   const authService = new AuthService();
   const expirationDays = parseInt(import.meta.env.VITE_COOKIE_EXPIRATION_DAYS);
-
+  
   const {
     register,
     handleSubmit,
