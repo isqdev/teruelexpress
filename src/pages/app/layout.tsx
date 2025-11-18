e17ad19bf58710461c87a3215e4be00fef3dd082
P.H.Lino <164282485+PedrooLino@users.noreply.github.com>
2025-11-18 18:51:28 -0300

    refactor(refinamento): retira partes desnecessarias

--- src/pages/app/layout.tsx
+++ src/pages/app/layout.tsx
@@ -6,12 +6,10 @@ import { Outlet } from "react-router-dom"
 export function Layout() {
  return (
     <SidebarProvider>
-      <div className="flex min-h-screen w-full">  
-        <AppSidebar />
-        <main className="flex-1">
-          <Outlet />
-        </main>
-      </div>
+          <AppSidebar/>
+          <main className="flex-1">
+            <Outlet />
+          </main>     
     </SidebarProvider>
   );
 }
